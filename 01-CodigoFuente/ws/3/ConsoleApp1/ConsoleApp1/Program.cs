434d2ddd5ecda3c14cce082161b79f5fb7db24f9
raulprogis <167821920+raulprogis@users.noreply.github.com>
2024-04-29 18:17:33 -0600

    se agregaron las actividades, y comentarios en la descripcion de funciones del la estructura de manu

---
+++ 01-CodigoFuente/ws/3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.ventanillaunica;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public ventanillaunica ws = new ventanillaunica();
+        static void Main(string[] args)
+        {
+        }
+    }
+}
