c9e3b820ce2db5d6afd0fb7c66b7586fed55be7d
raulprogis <167821920+raulprogis@users.noreply.github.com>
2024-05-08 18:14:44 -0600

    se e agrela las opciones para llamado del menu, se agrego  documentacion del flujo de la pantalla principal

--- 01-CodigoFuente/main/Spooler/main/main/DM.cs
+++ 01-CodigoFuente/main/Spooler/main/main/DM.cs
@@ -9,62 +9,76 @@ using System.Linq.Expressions;
 using System.Data.OracleClient;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
-namespace serverreports
+namespace serverreports;
+
+internal class DM
 {
-    internal class DM
+    public DataTable datos(string SQL)
     {
-        public DataTable datos(string SQL)
+        DataTable dtTemp = new DataTable();
+        OracleConnection cnn = new OracleConnection(conecBD());
+        using (cnn)
         {
-            DataTable dtTemp = new DataTable();
-            OracleConnection cnn = new OracleConnection(conecBD());
-            using (cnn)
+            cnn.Open();
+            if ((cnn.State) > 0)
             {
-                cnn.Open();
-                if ((cnn.State) > 0)
-                {
-                    OracleCommand cmd = new OracleCommand(SQL, cnn);
-                    OracleDataAdapter da = new OracleDataAdapter(cmd);
-                    //da = new OracleDataAdapter(cmd);
-                    da.Fill(dtTemp);
-                    cnn.Close();
-                }
+                OracleCommand cmd = new OracleCommand(SQL, cnn);
+                OracleDataAdapter da = new OracleDataAdapter(cmd);
+                da.Fill(dtTemp);
+                cnn.Close();
             }
-            return dtTemp;
         }
-        private string conecBD()
-        {
-            var configuration = new ConfigurationBuilder()
-                                              .AddUserSecrets(Assembly.GetExecutingAssembly())
-                                              .Build();
-            var orfeo = configuration["Orfeo2"];
-            return orfeo;
-        }
-        public DataTable Main_rep(string nom_proc, string id_cron, int vs)
-        {
-           DataTable dtTemp = new DataTable();
-           switch (nom_proc)
-            {
-                case "main_rp_cron":                         
-                                    break;
-            }
-            return dtTemp;
-        }
-
-        public  DataTable main_rp_cron(string id_cron, int vs)
-        {
-            string SQL = " select rep.id_rep, rep.ID_CRON, rep.NAME, rep.CONFIRMACION, rep.FRECUENCIA,     \n " +
-            " rep.cliente, cli.clistatus, cli.cliclef || ' - ' || InitCap(cli.clinom) cli_nom              \n " +
-            " from rep_detalle_reporte rep join eclient cli on cli.cliclef= rep.CLIENTE                    \n " +
-            " Where rep.ID_CRON =  @id_cron ";
-            DataTable dtTemp = new DataTable();
-            dtTemp = datos(SQL.Replace("@id_cron", "" + id_cron + ""));
-            return dtTemp;
-        }
-               
-
-
-
-
+        return dtTemp;
     }
+    private string conecBD()
+    {
+        var configuration = new ConfigurationBuilder()
+                                          .AddUserSecrets(Assembly.GetExecutingAssembly())
+                                          .Build();
+        var orfeo = configuration["Orfeo2"];
+        return orfeo;
+    }
+    public DataTable Main_rep(string nom_proc, string id_cron, int vs)
+    {
+        DataTable dtTemp = new DataTable();
+        switch (nom_proc)
+        {
+            case "main_rp_cron":
+                dtTemp = datos(main_rp_cron(id_cron.ToString(), vs));
+                break;
+            case "main_rep_detalle":
+                dtTemp = datos(main_rep_detalle(id_cron.ToString(), vs));
+                break;
+        }
+        return dtTemp;
+    }
+
+    public string /*DataTable*/ main_rp_cron(string id_cron, int vs)
+    {
+        string SQL = " select rep.id_rep, rep.ID_CRON, rep.NAME, rep.CONFIRMACION, rep.FRECUENCIA,     \n " +
+        " rep.cliente, cli.clistatus, cli.cliclef || ' - ' || InitCap(cli.clinom) cli_nom              \n " +
+        " from rep_detalle_reporte rep join eclient cli on cli.cliclef= rep.CLIENTE            \n " +
+        " Where rep.ID_CRON =  @id_cron ";
+        //DataTable dtTemp = new DataTable();
+        if (vs == 1) { Console.WriteLine(SQL.Replace("@id_cron", "" + id_cron + "") + "\n"); }
+        //dtTemp = datos(SQL.Replace("@id_cron", "" + id_cron + ""));
+         return SQL.Replace("@id_cron", "" + id_cron + "");
+        /*return dtTemp*/
+    }
+
+    public string main_rep_detalle(string id_cron, int vs)
+    {
+        string SQL = " select to_char(LAST_CONF_DATE_1, 'mm/dd/yyyy')  as fecha_1, to_char(LAST_CONF_DATE_2, 'mm/dd/yyyy') as fecha_2 "
+               + "  From rep_detalle_reporte where id_cron = @id_cron";
+        //DataTable dtTemp = new DataTable();
+        if (vs == 1) { Console.WriteLine(SQL.Replace("@id_cron", "" + id_cron + "") + "\n"); }
+        //dtTemp = datos(SQL.Replace("@id_cron", "" + id_cron + ""));
+        return SQL.Replace("@id_cron", "" + id_cron + "");
+        /*return dtTemp*/
+    }
+
+
+
+
 }
 
--- 01-CodigoFuente/main/Spooler/main/main/Program.cs
+++ 01-CodigoFuente/main/Spooler/main/main/Program.cs
@@ -19,14 +19,38 @@ if (id_cron != 0)
     Console.WriteLine("ID_CRON =" + id_cron);
     Console.WriteLine("reporte_temporal =" + reporte_temporal);
     DataTable trep_cron = new DataTable();
-    trep_cron = dM.main_rp_cron(id_cron.ToString(),0);
-  
-     if (trep_cron.Rows.Count>0)
+    //trep_cron = dM.main_rp_cron(id_cron.ToString(),0);
+    trep_cron = dM.Main_rep("main_rp_cron", id_cron.ToString(), 0);
+
+
+
+    if (trep_cron.Rows.Count > 0)
         Console.WriteLine(util.Tdetalle(trep_cron));
     else
         Console.WriteLine("Falta el numero del reporte.....");
+    /* por definir
+     If rs.EOF Then
+     GoTo Errman
+       ElseIf rs.Fields("id_rep") <> "317" And rs.Fields("clistatus") = "1" And reporte_temporal<> 1 And rs.Fields("cliente") <> "0" And(CLng(rs.Fields("cliente")) < 9900 Or CLng(rs.Fields("cliente")) > 9999) Then
+     Call send_error_mail("Error - Cliente inactivo - Report : < " & rs.Fields("NAME") & " >", mail_grupo_error, "El reporte tiene como cliente : " & rs.Fields("cli_nom") & " - " & " que es inactivo." & vbCrLf & "Favor de verificar lo y de quitar la programacion de este reporte.")
+     GoTo Errman
+     End If
+    */
+    DataTable tfec_conf = new DataTable();
+    if (reporte_temporal == 0)
+    {
+        string SQL = " select logis.display_fecha_confirmacion4(@param, sysdate, sysdate,1)  as fecha "
+                 + "  From dual";
+        tfec_conf = dM.datos(SQL.Replace("@param", "" + util.Tcampo(trep_cron, "FRECUENCIA") + ""));
+    }
+    else
+    {
+        tfec_conf = dM.Main_rep("main_rep_detalle", id_cron.ToString(), 0);
+    }
+    Console.WriteLine("display_fecha_confirmacion4 :" + util.Tdetalle(tfec_conf));
+    /****///
 }
 else
-        Console.WriteLine("Error es necesario dos parametros \n 1. Falta numero repor: ''{0}'' \n 2. valor numerico: {1} "+ msg, id_cron, reporte_temporal);
+    Console.WriteLine("Error es necesario dos parametros \n 1. Falta numero repor: ''{0}'' \n 2. valor numerico: {1} " + msg, id_cron, reporte_temporal);
 Console.WriteLine("Oprimar cualquier tecla para terminar");
 Console.ReadKey();
