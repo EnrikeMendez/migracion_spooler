605b1d2cd106301849dbf8f67f54e4d2cb35a998
raulprogis <167821920+raulprogis@users.noreply.github.com>
2024-05-16 17:58:11 -0600

    1. se documentaron:
    transmision_edocs_bosch
    Reservacion de Guias LTL txt
    Reservacion de Guias CD Excel
    ind_cal_bosch
    ing_egr_gar_pend_fact
    fondo_fijo
    back_aduana
    ind_cal_cli
    indice_cal_ad
      ruta \00-Documentacion\Listado_Modulos2.xlsx "descriptivo- detalle a migrar f"
    2. Se aplicaron los cambios en listado solicitado  Listado de reportes
    Ruta \00-Documentacion\activos.xlsx
    3. desglose de los sql , se les agrego comentario ruta
    Ruta 00-Documentacion\sql spooler\main
    4.Spooler Se publico “01-CodigoFuente\main\Spooler\main\main\publicc” , se agrego la función de inicialización variables, agregar al proceso la IP (función)  -

--- 01-CodigoFuente/main/Spooler/main/main/Program.cs
+++ 01-CodigoFuente/main/Spooler/main/main/Program.cs
@@ -1,18 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 using System.Linq.Expressions;
+using System.Net;
 using System.Security.Cryptography;
 using serverreports;
+using static System.Net.Mime.MediaTypeNames;
 int id_cron = 0;
 int sw_cron = 0;
-int num_of_param = 0;
-int visible_sql = 0;
+int visible_sql =0;
 string msg = "";
 string sqladd = " ,case when (@param=1 and  rep.FRECUENCIA is not null) then logis.display_fecha_confirmacion4(rep.FRECUENCIA, sysdate, sysdate,1)  end fecha  ";
 int reporte_temporal = 0;
 string FECHA_1 = "";
 string FECHA_2 = "";
-
+int num_of_param = 0;
+///init_var()
+string cc_mail = "";
+string mail_server = "";
+string mail_footer = "";
+string mail_From = "";
+string mail_FromName = "";
+string mail_grupo_error = "";
+string IP_servidor1 = "";
+string IP_servidor2 = "";
+string first_path = "";
+string second_path = "";
+string Get_IP = "";
+string mail_Lots_Info = "";
+bool mail_adjuntarArchivoXLS = false;
+bool mail_adjuntarArchivoTXT = false;
+string mail_tempFolder = "";
+bool bExit = false;
+///
 string reporte_name = "";
 int days_deleted = 0;
 string file_name = "";
@@ -23,16 +42,17 @@ string servidor = "";
 string param_string = "";
 string dest_mail = "";
 string MiComando = "";
-string first_path = "";
-
 
 Utilerias util=new Utilerias();
 DM DM = new DM();
+init_var();
 /*
  *Generar funsion de inicalicion de variables
  */
 string comand = args[0];
-try { id_cron = Convert.ToInt32(args[0]); } catch (Exception) {  msg = " ¡¡¡error opc de reporte¡¡"; }
+//try { id_cron = Convert.ToInt32(args[0]); } catch (Exception e) {  msg = " ¡¡¡error opc de reporte¡¡"; }
+try   { id_cron = Convert.ToInt32(args[0]); } catch (Exception e) { Console.WriteLine(e.Message); }
+
 if (args.Length == 2 && args[1] == "1")
     reporte_temporal = 1;
 DataTable trep_cron = new DataTable();
@@ -90,12 +110,12 @@ valida
     //////*******  Parametros *********////////////////////
     tmail_contact = DM.Main_rep("main_mail_contact", id_cron.ToString(), visible_sql);
     Console.WriteLine("************** SQL contactos **************");
-    Console.WriteLine(util.Tdetalle(tmail_contact));  
-    
+    Console.WriteLine(util.Tdetalle(tmail_contact));
+
 
     tnum_param = DM.Main_rep("main_num_param", id_cron.ToString(), visible_sql);
     num_of_param = Convert.ToInt32(util.Tcampo(tnum_param, "NUM_OF_PARAM"));
-    Console.WriteLine("Numero parametros :"+ num_of_param);
+    Console.WriteLine("Numero parametros :" + num_of_param);
     Console.WriteLine("************** parametros **************");
     Console.WriteLine(util.arma_param("REP.PARAM_", num_of_param));
 
@@ -112,11 +132,11 @@ valida
     }
 
     reporte_name = util.nvl(util.Tcampo(tdato_repor, "NAME"));
-    days_deleted = Int32.Parse(util.nvl(util.Tcampo(tdato_repor, "DAYS_DELETED")));
-    file_name = util.nvl(util.Tcampo(tdato_repor, "FILE_NAME"));
-    id_Reporte = Int32.Parse(util.nvl(util.Tcampo(tdato_repor, "ID_REP")));
-    //Carpeta = first_path & NVL(rs.Fields("CARPETA")) & "\" & IIf(NVL(rs.Fields("SUBCARPETA")) <> "", NVL(rs.Fields("SUBCARPETA")) & "\", "")
-    Carpeta = first_path + util.nvl(util.Tcampo(tdato_repor, "CARPETA"));
+    days_deleted = Int32.Parse(util.nvl(util.Tcampo(tdato_repor, "DAYS_DELETED"), "n"));
+    file_name    = util.nvl(util.Tcampo(tdato_repor, "FILE_NAME"));
+    id_Reporte   = Int32.Parse(util.nvl(util.Tcampo(tdato_repor, "ID_REP"), "n"));
+    //Carpeta      = first_path & NVL(rs.Fields("CARPETA")) & "\" & IIf(NVL(rs.Fields("SUBCARPETA")) <> "", NVL(rs.Fields("SUBCARPETA")) & "\", "")
+    Carpeta      = first_path + util.nvl(util.Tcampo(tdato_repor, "CARPETA"));
     if (util.nvl(util.Tcampo(tdato_repor, "SUBCARPETA")) != "")
         Carpeta = Carpeta + util.nvl(util.Tcampo(tdato_repor, "SUBCARPETA")) + "\\";
     else
@@ -131,8 +151,58 @@ valida
     Console.WriteLine("valor ''id_Reporte  '':" + id_Reporte);
     Console.WriteLine("valor ''Carpeta     '':" + Carpeta);
     Console.WriteLine("valor ''COMMAND     '' " + MiComando);
+    Console.WriteLine("valor ''Get_IP         '' " + Get_IP);
+    Console.WriteLine("valor ''first_path     '' " + first_path);
+    Console.WriteLine("valor ''second_path    '' " + second_path);
+    /*
+                 
+             IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+             foreach (IPAddress ip in host.AddressList)
+             {
+                 if (ip.AddressFamily.ToString() == "InterNetwork")
+                 {
+                     localIP = ip.ToString();
+                 }
+             }
+             Console.WriteLine("valor IP " + localIP);
+             return localIP;
+     */
+
 }
 else
     Console.WriteLine("Error es necesario dos parametros \n 1. Falta numero repor: ''{0}'' \n 2. valor numerico: {1} " + msg, id_cron, reporte_temporal);
 Console.WriteLine("Oprimar cualquier tecla para terminar");
 Console.ReadKey();
+
+void init_var()
+{
+    num_of_param = 0;
+    cc_mail = "";
+    mail_server = "192.168.100.6";
+    mail_footer = "\n" + "\n" + "\n" +
+    "*********************************************************\n" +
+    "This is a message automatically generated, please contact \n" +
+    "web-master@logis.com.mx for any question or to unsubscribe.";
+    mail_From = "web-reports@logis.com.mx";
+    mail_FromName = "Logis report server";
+    mail_grupo_error = "desarrollo_web@logis.com.mx;christelle@logis.com.mx;desarrollo_web@logis.com.mx;desarrollo_web@logis.com.mx;";
+    IP_servidor1 = "192.168.100.5";
+    IP_servidor2 = "192.168.100.4";
+    first_path = "\\\\" + IP_servidor1 + "\\reportes\\web_reports\\";
+    second_path = "\\\\" + IP_servidor2 + "\\reportes\\web_reports\\";
+    Get_IP = util.Get_IP();
+    first_path = "\\\\" + Get_IP + "\\reportes\\web_reports\\";
+    if (Get_IP == IP_servidor1)
+        second_path = "\\\\" + IP_servidor2 + "\\reportes\\web_reports\\";
+    else
+        second_path = "\\\\" + IP_servidor1 + "\\reportes\\web_reports\\";
+    mail_Lots_Info = "";
+    mail_adjuntarArchivoXLS = false;
+    mail_adjuntarArchivoTXT = false;
+    mail_tempFolder = "\\\\192.168.100.4\\reportes\\web_reports\\temp\\";
+    bExit = false;
+}
+void Errman(Exception e)
+{
+
+}
--- 01-CodigoFuente/main/Spooler/main/main/Utilerias.cs
+++ 01-CodigoFuente/main/Spooler/main/main/Utilerias.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,6 @@ namespace serverreports
 {
     internal class Utilerias
     {
-
         public string Tdetalle(DataTable dtTemp)
         {
             string tit = "";
@@ -53,13 +53,37 @@ namespace serverreports
             }
             return valor;
         }
-
-        public string nvl(string cadena)
+        public string nvl(string cadena, string? tp = "s")
         {
-            if (string.IsNullOrEmpty(cadena))
-                return "";
+            if (tp.ToUpper() == "S")
+            {
+                if (string.IsNullOrEmpty(cadena))
+                    return "";
+                else
+                    return cadena;
+            }
             else
-                return cadena;
+            {
+                if (string.IsNullOrEmpty(cadena))
+                    return "0";
+                else
+                    return cadena;
+            }
+        }
+        public string Get_IP()
+        {                
+           string localIP = "";
+          ///labdda funcion coalesce -- int sp = localIP.ToString() == null ? 1 : 0;
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+           {
+               if (ip.AddressFamily.ToString() == "InterNetwork")
+               {
+                   localIP = ip.ToString();
+               }
+           }
+           return localIP;          
         }
     }
+
 }
