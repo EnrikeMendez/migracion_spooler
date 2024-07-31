2ff9e6cb80456d7d15f48e2e0bf76a2471d40bb3
raulprogis <167821920+raulprogis@users.noreply.github.com>
2024-07-30 18:04:21 -0600

    Script de Ing_egr_gar_pend_fact_mod
    -EGR_ING_Pend_Fact
    -Folios
    -web_indice_cal_bosch

--- 01-CodigoFuente/main/Spooler/main/main/obj/Debug/net8.0/serverreports.AssemblyInfo.cs
+++ 01-CodigoFuente/main/Spooler/main/main/obj/Debug/net8.0/serverreports.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverreports")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da8f102163101e98a704e4b29780286e7a3e7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverreports")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverreports")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
