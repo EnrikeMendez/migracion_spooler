607c2e994cdbcd74f92f025aa3fecad9e3bef808
raulprogis <167821920+raulprogis@users.noreply.github.com>
2024-08-30 17:50:25 -0600

    Spooler 2024 Fondo_fijo
    Terminación de reporte generación correo.
    actualizacion de fechas

--- 01-CodigoFuente/main/Spooler/main/main/web_fondo_fijo_mod.cs
+++ 01-CodigoFuente/main/Spooler/main/main/web_fondo_fijo_mod.cs
@@ -35,8 +35,8 @@ namespace serverreports
             par_st[0, 0] = "i";
             par_st[0, 1] = "v";
             par_st[0, 2] = "p_prep_clave";
-            //par_st[0, 3] = rep_clave;
-             par_st[0, 3] = "535";
+            par_st[0, 3] = rep_clave;
+            //par_st[0, 3] = "535";
 
             par_st[1, 0] = "i";
             par_st[1, 1] = "i";
@@ -46,20 +46,24 @@ namespace serverreports
 
             par_st[2, 0] = "i";
             par_st[2, 1] = "v";
-            //par_st[2, 2] = "pdiv";
-             par_st[2, 3] = "MXN";
-            par_st[3, 3] = Divisa;
+            par_st[2, 2] = "pdiv";
+            // par_st[2, 3] = "MXN";
+            par_st[2, 3] = Divisa;
+
             par_st[3, 0] = "o";
             par_st[3, 1] = "c";
             par_st[3, 2] = "p_Cur_Fondo_Fijo";
+
             par_st[4, 0] = "o";
             par_st[4, 1] = "v";
             par_st[4, 2] = "p_MENSAJE";
             par_st[4, 3] = "msg";
+
             par_st[5, 0] = "o";
             par_st[5, 1] = "i";
             par_st[5, 2] = "p_CODIGO_ERROR";
             par_st[5, 3] = "cod";
+
             datos_sp.sql = "SC_RS.SPG_RS_COEX_DAF_REPORTES.P_DAT_FONDO_FIJO";
             datos_sp = DM.datos_sp([datos_sp.sql], par_st, vs);
             LisDT[0] = datos_sp.tb;
@@ -67,30 +71,18 @@ namespace serverreports
             inf.LisDT_tit = LisDT_tit;
             inf.LisDT = LisDT;
             inf.arch = arch;
-
+            /*
             arh[0] = Carpeta + "\\" + file_name[0, 0] + ".xlsx";
             file_name[0, 0] = file_name[0, 0] + ".xlsx";
-            //if (file_name[4, 0] == "1")
-            //   arh[1] = util.agregar_zip(arh, file_name[0, 0], Carpeta);
             if (file_name[4, 0] == "1")
             {
-                //arh[1] = util.agregar_zip_nv(file_name, arch, Carpeta);
                 html = util.agregar_zip(file_name, arch, Carpeta);
                 arh[1] = Carpeta + "\\" + arch + ".zip";
             }
-
+            */
             //file_name[0, 0] = file_name[0, 0] + ".xlsx";
-            html = util.hexafile_nv(file_name, Carpeta, int.Parse(parins[9, 1]), arch, parins);
-            util.replica_tem(arch, parins);
-            string warning_message = DM.msg_temp(parins, visible_sql);
-            string mensaje = correo.display_mail(parins[10, 1], warning_message, arch, html, Int32.Parse(parins[3, 1]), "");
-            //  correo.send_mail("Report: < Logis transmision_edocs_bosch > Envio ok", [], "proceso correcto", [Carpeta + "\\" + file_name + ".xlsx"]);
-            if (contacmail.Length > 0)
-            {
-                correo.send_mail("Report: <" + html[1, 0] + "> created v2024", [], mensaje, arh);
-            }
-            DM.act_proceso(parins, visible_sql);
-            util.borra_arch(arh, Carpeta);
+            //html = util.hexafile_nv(file_name, Carpeta, int.Parse(parins[9, 1]), arch, parins);
+            //util.replica_tem(arch, parins);
             return inf;
         }
         public string Fondo_fijo_ant(string Archivo, string Empresa, string Divisa,  string[,] parins, string[] contacmail, int vs)
@@ -173,7 +165,7 @@ namespace serverreports
                 Directory.CreateDirectory(cp);
             }
             DateTime DateTime = DateTime.Now;
-            using (StreamWriter sw = File.CreateText(cp + " Detail_fonfofijo.txt"))
+            using (StreamWriter sw = File.CreateText(cp + "Detail_fonfofijo.txt"))
             {
                 sw.WriteLine(util.Tdetalle(datos_sp.tb));
             }
